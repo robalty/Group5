2a3a7d1616f2d9d14141668949ba7dc80c6207ae
thongtran715 <thongtran715@gmail.com>
2017-11-20 21:44:05 -0800

    List File

--- src/com/company/List_service.java
+++ src/com/company/List_service.java
@@ -6,8 +6,17 @@ package com.company;
 
 
 enum List_Service_Type {
-
+        all_services_available_provider, all_services_provided_provider, all_services_received_customer
 }
 
 public class List_service {
+        private Service head;
+        private Service tail;
+        private List_Service_Type type;
+        public List_service  (){
+               this.head = null;
+               this.tail = null;
+        }
+
+
 }
